589a435fa8564fe439e28aed7a51daf482747d1d
Artem Borisov <dedsa2002@gmail.com>
2019-05-14 00:38:49 +0530

    msm8917-common: Fix USB config extra property
    * It should be persist.vendor.usb.config.extra, not persist.vendor.sys.usb.config.extra
    
    Change-Id: Icc5531c5563f1975314be38f5f7567e1c0f5eb8d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -167,7 +167,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.sys.usb.config.extra=none
+    persist.vendor.usb.config.extra=none
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
